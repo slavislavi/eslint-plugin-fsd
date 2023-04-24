5df7ea38b68272958c9c8bed7849011b65e8d67b
slavislavi <slavislavi1986@gmail.com>
2023-04-24 15:07:53 +0300

    [main]: public-api-imports-rule-with-testing

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -7,66 +7,30 @@ const ruleTester = new RuleTester({
     parserOptions: { ecmaVersion: 6, sourceType: 'module' }
 });
 
-const aliasOptions = [
-    {
-        alias: '@'
-    }
-];
-
-ruleTester.run("public-api-imports", rule, {
+ruleTester.run("path-checker", rule, {
     valid: [
-        {
-            code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
-            errors: [],
-        },
-        {
-            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article'",
-            errors: [],
-            options: aliasOptions,
-        },
-        {
-            filename: 'C:\\Users\\slavio\\Desktop\\javascript\\production_project\\src\\entities\\file.test.ts',
-            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
-            errors: [],
-            options: [{
-                alias: '@',
-                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
-            }],
-        },
-        {
-            filename: 'C:\\Users\\slavio\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
-            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
-            errors: [],
-            options: [{
-                alias: '@',
-                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
-            }],
-        }
+      {
+        filename: 'C:\\Users\\slavio\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+        code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+        errors: [],
+      },
     ],
-
+  
     invalid: [
-        {
-            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/file.ts'",
-            errors: [{ message: "Absolute import is allowed from Public API file only (index.ts)" }],
-            options: aliasOptions,
-        },
-        {
-            filename: 'C:\\Users\\slavio\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
-            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx'",
-            errors: [{ message: "Absolute import is allowed from Public API file only (index.ts)" }],
-            options: [{
-                alias: '@',
-                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
-            }],
-        },
-        {
-            filename: 'C:\\Users\\slavio\\Desktop\\javascript\\production_project\\src\\entities\\forbidden.ts',
-            code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
-            errors: [{ message: "Absolute import is allowed from Public API file only (index.ts)" }],
-            options: [{
-                alias: '@',
-                testFilesPatterns: ['**/*.test.ts', '**/*.test.ts', '**/StoreDecorator.tsx']
-            }],
-        }
+      {
+        filename: 'C:\\Users\\slavio\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+        code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+        errors: [{ message: "All paths must be relative within the slice"}],
+        options: [
+          {
+            alias: '@'
+          }
+        ]
+      },
+      {
+        filename: 'C:\\Users\\slavio\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+        code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+        errors: [{ message: "All paths must be relative within the slice"}],
+      },
     ],
-});
+  });
--- tests/lib/rules/public-api-imports.js
+++ tests/lib/rules/public-api-imports.js
@@ -1,21 +1,72 @@
 "use strict";
 
 const rule = require("../../../lib/rules/public-api-imports"),
-    RuleTester = require("eslint").RuleTester;
+  RuleTester = require("eslint").RuleTester;
 
 const ruleTester = new RuleTester({
     parserOptions: { ecmaVersion: 6, sourceType: 'module' }
 });
 
+const aliasOptions = [
+    {
+        alias: '@'
+    }
+];
+
 ruleTester.run("public-api-imports", rule, {
     valid: [
-        // give me some code that won't trigger a warning
+      {
+        code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+        errors: [],
+      },
+      {
+        code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article'",
+        errors: [],
+        options: aliasOptions,
+      },
+      {
+        filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\file.test.ts',
+        code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+        errors: [],
+        options: [{
+          alias: '@',
+          testFilesPatterns: ['**/*.test.ts', '**/*.stories.ts', '**/StoreDecorator.tsx']
+        }],
+      },
+      {
+        filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
+        code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+        errors: [],
+        options: [{
+          alias: '@',
+          testFilesPatterns: ['**/*.test.ts', '**/*.stories.ts', '**/StoreDecorator.tsx']
+        }],
+      }
     ],
-
+  
     invalid: [
-        {
-            code: "",
-            errors: [{ message: "Fill me in.", type: "Me too" }],
-        },
+      {
+        code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/file.ts'",
+        errors: [{ message: "Absolute import is allowed from Public API file only (index.ts)" }],
+        options: aliasOptions,
+      },
+      {
+        filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\StoreDecorator.tsx',
+        code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx'",
+        errors: [{ message: "Absolute import is allowed from Public API file only (index.ts)" }],
+        options: [{
+          alias: '@',
+          testFilesPatterns: ['**/*.test.ts', '**/*.stories.ts', '**/StoreDecorator.tsx']
+        }],
+      },
+      {
+        filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\forbidden.ts',
+        code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
+        errors: [{ message: "Test data is needed to import from publicApi/testing.ts" }],
+        options: [{
+          alias: '@',
+          testFilesPatterns: ['**/*.test.ts', '**/*.stories.ts', '**/StoreDecorator.tsx']
+        }],
+      }
     ],
-});
+  });
